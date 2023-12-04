0dad77d1dbb4389c8ee0e59d7394124356f420a8
just-hms <alessandroversari1@gmail.com>
2023-12-05 00:39:37 +0100

    add: about page

---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex flex-col items-center justify-center">
+    <figure class="max-w-lg mx-auto group">
+        <div class="relative">
+            <img
+                src="tmp.jpeg"
+                alt="Background"
+                class="h-auto max-w-full rounded-lg transition duration-500 group-hover:sepia-[0.3] group-hover:contrast-[0.8]"
+            />
+            <div
+                class="top-0 left-0 w-full h-full absolute opacity-0 transition duration-500 group-hover:opacity-80 bg-repeat bg-[length:30rem_8rem] contrast-125 drop-shadow-lg"
+                style="background-image: url('prison.jpg');"
+            ></div>
+        </div>
+        <figcaption
+            class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400"
+        >
+            This is us
+        </figcaption>
+    </figure>
+</div>
